93ca37af522e1ec7ba8026376a68e0153e1f0929
gabrovnikov <gabrvalete@gmail.com>
2023-02-13 13:32:11 -0300

    Separação dos arquivos Js e HTML

---
+++ Js/script.js
@@ -0,0 +1,55 @@
+const form = document.getElementById('form');
+    //const campos = document.querySelectorAll('.required');
+    const name = document.querySelector('#name');
+    const descricao = document.querySelector('#descricao');
+    const spanName = document.querySelector('#span-name');
+    const spanDescricao = document.querySelector('#span-descricao');
+
+    function descricaoValidate(){
+
+if(descricao.value.length < 16){
+
+    console.log("Bug/Sugestão deve ser maior que 10 caracteres");
+    setError();
+}
+
+else {
+    console.log("Validado");
+    removeError();
+}
+
+
+function setError(){
+
+    descricao.style.border='2px solid #e63636';
+    spanDescricao.style.display = 'block';
+    
+}
+
+function removeError(){
+    descricao.style.border='';
+    spanDescricao.style.display = 'none';
+}
+
+}
+
+
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+
+        if(name.value === ""){
+            alert("Por favor, preencha seu nome");
+            return;
+        }
+
+        
+        
+
+        if(descricao.value.length < 11){
+            alert("Bug/Sugestão deve ser maior que 10 caracteres");
+            return;
+        }
+
+        form.submit();
+
+    })
