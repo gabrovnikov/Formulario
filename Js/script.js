df94ff8e7f4c5e72a03e006f6e7a1aea62d72dc5
gabrovnikov <gabrvalete@gmail.com>
2023-02-13 15:42:03 -0300

    Correção no alert

--- Js/script.js
+++ Js/script.js
@@ -1,55 +1,42 @@
-const form = document.getElementById('form');
-    //const campos = document.querySelectorAll('.required');
-    const name = document.querySelector('#name');
-    const descricao = document.querySelector('#descricao');
-    const spanName = document.querySelector('#span-name');
-    const spanDescricao = document.querySelector('#span-descricao');
+const form = document.getElementById("form");
+//const campos = document.querySelectorAll('.required');
+const name = document.querySelector("#name");
+const descricao = document.querySelector("#descricao");
+const spanName = document.querySelector("#span-name");
+const spanDescricao = document.querySelector("#span-descricao");
 
-    function descricaoValidate(){
-
-if(descricao.value.length < 16){
-
-    console.log("Bug/Sugestão deve ser maior que 10 caracteres");
+function descricaoValidate() {
+  if (descricao.value.length < 16) {
+    console.log("Bug/Sugestão deve ser maior que 15 caracteres");
     setError();
-}
-
-else {
+  } else {
     console.log("Validado");
     removeError();
+  }
+
+  function setError() {
+    descricao.style.border = "2px solid #e63636";
+    spanDescricao.style.display = "block";
+  }
+
+  function removeError() {
+    descricao.style.border = "";
+    spanDescricao.style.display = "none";
+  }
 }
 
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
 
-function setError(){
+  if (name.value === "") {
+    alert("Por favor, preencha seu nome");
+    return;
+  }
 
-    descricao.style.border='2px solid #e63636';
-    spanDescricao.style.display = 'block';
-    
-}
+  if (descricao.value.length < 11) {
+    alert("Bug/Sugestão deve ser maior que 10 caracteres");
+    return;
+  }
 
-function removeError(){
-    descricao.style.border='';
-    spanDescricao.style.display = 'none';
-}
-
-}
-
-
-    form.addEventListener("submit", (event) => {
-        event.preventDefault();
-
-        if(name.value === ""){
-            alert("Por favor, preencha seu nome");
-            return;
-        }
-
-        
-        
-
-        if(descricao.value.length < 11){
-            alert("Bug/Sugestão deve ser maior que 10 caracteres");
-            return;
-        }
-
-        form.submit();
-
-    })
+  form.submit();
+});
